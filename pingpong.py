50ad3146aad92109df3aa4f8e20f495a6efeab16
kybermartin <spak.martin@gmail.com>
2024-06-12 13:26:00 +0200

    vytvoril som triedu Player a pridal som objekty hracov do hry

--- pingpong.py
+++ pingpong.py
@@ -9,6 +9,31 @@ RED = (255, 0, 0)
 GREEN = (0,255,0)
 FPS = 40
 
+#trieda Player
+class Player(sprite.Sprite):
+    def __init__(self, x, y, width, height, speed, color):
+        super().__init__()
+        self.rect = draw.rect(window, color, (x, y, width, height))
+        self.speed = speed
+        self.color = color
+
+    def right_move(self):
+        pressed = key.get_pressed()
+        if pressed[K_p] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if pressed[K_l] and self.rect.y < WIN_HEIGHT - self.rect.height:
+            self.rect.y += self.speed
+    
+    def left_move(self):
+        pressed = key.get_pressed()
+        if pressed[K_q] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if pressed[K_a] and self.rect.y < WIN_HEIGHT - self.rect.height:
+            self.rect.y += self.speed
+         
+    def draw(self):
+        draw.rect(window, self.color, (self.rect))
+
 #premenne hry
 run = True
 
@@ -19,12 +44,22 @@ display.set_caption("Ping Pong")
 #casovac hry
 clock = time.Clock()
 
+#objekty hry
+player1 = Player(5,100, 30, 150, 10, RED)
+player2 = Player(WIN_WIDTH - 35, 100, 30, 150, 10, GREEN)
+
 #herna slucka
 while run:
 
     for e in event.get():
         if e.type == QUIT:
             run = False
+
+    window.fill(BLACK)
+    player1.left_move()
+    player2.right_move()
+    player1.draw()
+    player2.draw()
 
     display.update()
     clock.tick(FPS)
