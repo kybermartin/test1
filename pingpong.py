abb910560185acab78d55a67c740dc56726472f2
kybermartin <spak.martin@gmail.com>
2024-06-21 13:18:52 +0200

    doplnil som kolizie

--- pingpong.py
+++ pingpong.py
@@ -47,7 +47,7 @@ class Ball(sprite.Sprite):
         self.rect = draw.circle(window, color, (x,y), radius)
 
     def update(self):
-        if self.rect.y < 0 or self.rect.y > WIN_HEIGHT:
+        if self.rect.y - self.radius < 0 or self.rect.y + self.radius > WIN_HEIGHT:
             self.y_speed *= -1
 
         self.rect.x += self.x_speed
@@ -56,8 +56,28 @@ class Ball(sprite.Sprite):
     def draw(self):
         draw.circle(window, self.color, (self.rect.x,self.rect.y), self.radius)
 
+# trieda kolizii
+class CollideManager():
+    def __init__(self, player1, player2, ball):
+        #kolizie
+        self.player1 = player1
+        self.player2 = player2
+        self.ball = ball
 
 
+    def update(self):
+        if sprite.collide_rect(self.ball, self.player2):
+            ball.x_speed *= -1
+            ball.rect.right = player2.rect.left
+            
+        if sprite.collide_rect(self.ball, self.player1):
+            ball.x_speed *= -1
+            ball.rect.left = player1.rect.right
+            
+        if ball.rect.x > WIN_WIDTH:            
+            pass
+        if ball.rect.x < 0:
+            pass
 
 #premenne hry
 run = True
@@ -72,8 +92,8 @@ clock = time.Clock()
 #objekty hry
 player1 = Player(5,100, 30, 150, 10, RED)
 player2 = Player(WIN_WIDTH - 35, WIN_HEIGHT - 200, 30, 150, 10, GREEN)
-ball = Ball(WIN_WIDTH // 2, WIN_HEIGHT // 2, 10, 5, WHITE)
-
+ball = Ball(WIN_WIDTH // 2, WIN_HEIGHT // 2, 10, 4, WHITE)
+collide = CollideManager(player1, player2, ball)
 
 #herna slucka
 while run:
@@ -81,27 +101,15 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-#kolizie
-    '''
-    if sprite.collide_rect(ball, player2):
-        ball.directionX *= -1
-    
-    if sprite.collide_rect(player1, ball):
-        ball.directionX *= -1
-    
-    if ball.rect.x > WIN_WIDTH:
-        victory.set_victory("Red Player")
-    
-    if ball.rect.x < 0:
-        victory.set_victory("Green Player")
-    '''
 
     window.fill(BLACK)
+    collide.update()
     player1.left_move()
     player2.right_move()
     ball.update()
     player1.draw()
     player2.draw()
+    
     ball.draw()
 
     display.update()
